b3ae3242ef04db841e4e3c2a4d556c5596bd8b6d
Karl Horky <karl.horky@gmail.com>
2025-06-08 13:18:02 +0200

    Fix @react-pdf/renderer "Element type is invalid" error in terminal

--- src/components/invoices/preview/download-pdf-button.tsx
+++ src/components/invoices/preview/download-pdf-button.tsx
@@ -1,11 +1,19 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-import { PDFDownloadLink } from "@react-pdf/renderer";
-
 import { type InsertOrganizationType } from "@/lib/db/schema/organization";
 import { type InvoiceFormType } from "@/lib/form-schema/invoice-form";
 
+// Import via relative node_modules path to avoid "Element type
+// is invalid" error in the terminal when importing directly from
+// @react-pdf/renderer in a Next.js app
+//
+// ```
+// Error: Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined
+// ```
+//
+// - https://github.com/diegomura/react-pdf/issues/2890#issuecomment-2443831013
+import { PDFDownloadLink } from "../../../../node_modules/@react-pdf/renderer/lib/react-pdf.browser";
 import { PdfDocument } from "./pdf-preview";
 
 interface Props {
