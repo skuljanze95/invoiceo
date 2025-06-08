bff1d94591a67dc3b281186011e5ea3a7bdb3dd2
Karl Horky <karl.horky@gmail.com>
2025-06-08 13:18:45 +0200

    Remove unnecessary isClient useEffect wrapping

--- src/components/invoices/preview/download-pdf-button.tsx
+++ src/components/invoices/preview/download-pdf-button.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { type InsertOrganizationType } from "@/lib/db/schema/organization";
 import { type InvoiceFormType } from "@/lib/form-schema/invoice-form";
@@ -23,25 +23,13 @@ interface Props {
 }
 
 export function DownloadPdfButton({ children, invoice, organization }: Props) {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
-    <>
-      {isClient ? (
-        <PDFDownloadLink
-          document={
-            <PdfDocument invoice={invoice} organization={organization} />
-          }
-          className="-mt-4"
-          fileName={invoice.invoiceId ?? "invoice"}
-        >
-          {children}
-        </PDFDownloadLink>
-      ) : null}
-    </>
+    <PDFDownloadLink
+      className="-mt-4"
+      document={<PdfDocument invoice={invoice} organization={organization} />}
+      fileName={invoice.invoiceId ?? "invoice"}
+    >
+      {children}
+    </PDFDownloadLink>
   );
 }
